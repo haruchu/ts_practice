ce6246ccf0f09852f65dbc072a4fb2c98e14d952
haru <haruki6180@gmail.com>
2022-01-20 01:33:38 +0900

    class

--- object.ts
+++ object.ts
@@ -1,9 +1,13 @@
-const person: {
+class Person {
   name: string;
   age: number;
-} = {
-  name: 'haru',
-  age: 98
-};
 
-console.log(person)
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const p1 = new Person('haru', 98);
+
+console.log(p1)
